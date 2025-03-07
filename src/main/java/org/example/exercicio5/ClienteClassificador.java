44e6a30bdfe296a5be58528194f13a123e9dad32
Andre Ferrarez <andre.augusto.17@gmail.com>
2025-03-07 10:43:41 -0300

    Exercicio 5 corrigido

--- src/main/java/org/example/exercicio5/ClienteClassificador.java
+++ src/main/java/org/example/exercicio5/ClienteClassificador.java
@@ -4,7 +4,7 @@ public class ClienteClassificador {
 
     public String classificarCliente(int idade, double renda, int compras, String localizacao) {
 
-        String tipoIdade = idade > 60 ? "Senior" : "Jovem";
+        String tipoIdade = idade > 60 ? "Sênior" : "Jovem";
 
         double limiteRenda = idade > 60 ? 5000 : 7000;
         int limiteCompras = idade > 60 ? 10 : 20;
@@ -12,9 +12,10 @@ public class ClienteClassificador {
         if (renda > limiteRenda && compras > limiteCompras) {
             return "Cliente Premium " + tipoIdade;
         } else if (renda > limiteRenda) {
-            return "Cliente Jovem " + tipoIdade;
-        }else {
-            return "Cliente Jovem Baixa renda " + tipoIdade;
+            return "Cliente Regular " + tipoIdade;
+        } else {
+            return "Cliente " + tipoIdade + " Baixa Renda";
         }
     }
 }
+
